5f91c79cd9d55f9179edce13c04e79d2863c3771
DELL <rahulmishra0117@gmail.com>
2021-07-04 05:05:22 -0500

    adding all  the backend part

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,29 @@
 const mongoose=require('mongoose');
+// yaha atlas use krna pdaga
 
-mongoose.connect('mongodb://localhost/GreenBord');
 
-const db=mongoose.connection;
+mongoose.connect('mongodb+srv://Rahul:rahul@mycluster0.v5bvh.mongodb.net/Todo_app?retryWrites=true&w=majority',
+{useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost/todo_app');
+const db = mongoose.connection;
+//mongoose.connect(url);
+
+// try {
+//     // Connect to the MongoDB cluster
+//      mongoose.connect(
+//       url,
+//       { useNewUrlParser: true, useUnifiedTopology: true },
+//       () => console.log(" Mongoose is connected")
+//     );
+
+//   } catch (e) {
+//     console.log("could not connect");
+//   }
+
+
+
+// const db=mongoose.connection;
+
 db.on('error',console.error.bind(console,'error is find'));
 
 db.once('open',function(){
